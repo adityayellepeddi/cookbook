41919a3aa8c7c633c433557d21b44f20ff938fd7
root <root@ip-172-31-16-103.us-east-2.compute.internal>
2018-04-18 17:04:46 +0000

    .erb not working

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -1,35 +1,18 @@
+package 'nano'
+# or package 'vim-enhanced'
+package 'ntp'
+
 package 'tree' do
-   action :install
+	action :install
 end
-
-package 'vim'
-
-package 'emacs'
-
-
-package 'ntp' do
-   action :install
-end
-
-
-
 
 package 'git' do
-   action:install
+	action :install
+end
+
+template '/etc/motd' do
+  source 'motd.erb'
+  action :create
 end
 
 
-
-
-service 'ntpd' do
-    action [:enable, :start]
-end
-
-
-
-
-file 'etc/motd' do
-   content 'property of Aditya Yellepeddi'
-   owner 'root'
-   group 'root'
-end
